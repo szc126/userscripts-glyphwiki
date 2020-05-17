5e7a92d8461d1cadb6bea4d2586acd6b28b4ab3f
szc126 <szc126@users.noreply.github.com>
2020-05-16 18:18:36 -0700

    [gwUnicodeGlyphNames] v2, use global functions

--- gwUnicodeGlyphNames.user.js
+++ gwUnicodeGlyphNames.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        GlyphWiki: convert Unicode codepoints to Unicode in glyph names
-// @version     1
+// @version     2
 // @namespace   szc
 // @description -
 // @match       *://glyphwiki.org/wiki/*
 // @match       *://*.glyphwiki.org/wiki/*
 // @run-at      document-idle
 // @grant       none
+// @inject-into content
 // ==/UserScript==
 
 let as = document.querySelectorAll('a[href*="/wiki/u"]:not([href*="action="])'); // good enough
@@ -15,10 +16,7 @@ for (let i = 0; i < as.length; i++) {
 		let textOrig = as[i].childNodes[j].textContent;
 		let textUni = textOrig;
 
-		textUni = textUni.replace(/-?u([0-9a-f]{4,})/g, function(_, m1) {
-			return String.fromCodePoint('0x' + m1);
-		});
-		textUni = textUni.replace(/-?cdp-([0-9a-f]{4,})/g, '〓');
+		textUni = unsafeWindow.SH.nameToUnicode(textUni);
 
 		if (textOrig != textUni) {
 			let ruby = document.createElement('ruby');
